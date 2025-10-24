8e05c1a7ea0480ed50d10124d6cb21a3c51a26bc
감사감 <kokio02@pusan.ac.kr>
2025-10-24 20:31:22 +0900

    feat(Input): 자동차 이름, 라운드 횟수 입력 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
   async run() {}
+  getCarNamesInput() {
+    return MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+  }
+  
+  getRoundInput() {
+    return MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+  }
 }
 
 export default App;
